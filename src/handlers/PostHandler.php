a46b6a33118a78fef1a22d2f0a740d69226804ea
lucasdgimenez <lucas_d360@hotmail.com>
2023-02-27 20:46:38 -0300

    feed check

---
+++ src/handlers/PostHandler.php
@@ -0,0 +1,81 @@
+<?php
+namespace src\handlers;
+
+use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelation;
+
+class PostHandler {
+    public static function addPost($idUser, $type, $body) {
+        $body = trim($body);
+
+        if(!empty($idUser) && !empty($body)) {
+            Post::insert([
+                'id_user' => $idUser,
+                'type' => $type,
+                'created_at' => date('Y-m-d H:i:s'),
+                'body' => $body
+            ])->execute();
+        }
+    }
+
+    public static function getHomeFeed($idUser, $page) {
+        $perPage = 2;
+
+        //1. pegar listas de usuario que o user logado segue
+        $userList = UserRelation::select()->where('user_from', $idUser)->get();
+        $users = [];
+        foreach ($userList as $userItem) {
+            $users[] = $userItem['user_to'];
+        }
+        $users[] = $idUser;
+
+        //2. pegar os posts dessa turma ordenado
+        $postList = Post::select()
+            ->where('id_user', 'in', $users)
+            ->orderBy('created_at', 'desc')
+            ->page($page, $perPage)
+        ->get();
+
+        $total = Post::select()
+            ->where('id_user', 'in', $users)
+        ->count();
+        $pageCount = ceil($total / $perPage);
+
+        //3. transformar em objeto
+        $posts = [];
+        foreach ($postList as $postItem) {
+            $newPost = new Post();
+            $newPost->id = $postItem['id'];
+            $newPost->type = $postItem['type'];
+            $newPost->created_at = $postItem['created_at'];
+            $newPost->body = $postItem['body'];
+            $newPost->mine = false;
+
+            if($postItem['id_user'] == $idUser) {
+                $newPost->mine = true;
+            }
+
+            $newUser = User::select()->where('id', $postItem['id_user'])->one();
+            $newPost->user = new User();
+            $newPost->user->id = $newUser['id'];
+            $newPost->user->name = $newUser['name'];
+            $newPost->user->avatar = $newUser['avatar'];
+
+            //LIKE
+            $newPost->likeCount = 0;
+            $newPost->liked = false;
+            //COMMENTS
+            $newPost->comments = [];
+            
+            $posts[] = $newPost;
+        }
+        //4. preencher as informações adicionais
+        //5. retornar
+        return [
+            'posts' => $posts,
+            'pageCount' => $pageCount,
+            'currentPage' => $page
+        ];
+    }
+}
